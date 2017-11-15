18020b9b9ac550e3c28087f430afe7659dce2b3d
Henning Meyer <tutmann@tutmann1.roam.corp.google.com>
2017-11-16 00:12:59 +0100

    manually write to stdout
    this proves we understand aalibs rendering result

--- ascii.cpp
+++ ascii.cpp
@@ -2,20 +2,44 @@
 #include <aalib.h>
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
 
 using namespace std;
 using namespace cv;
 
-Ascii::Ascii() :p_(new aa_renderparams({47, 33, 4.0, AA_FLOYD_S, 1, 0})) {
-  aa_recommendhidisplay("linux");
+class StreamAttr {
+ public:
+  explicit StreamAttr(std::ostream* stream) : stream_(stream) {}
+  void set(int attr) {
+    if (attr == prev_) return;
+    prev_ = attr;
+    switch (attr) {
+      // Bold off
+      case 0: *stream_ << "\e[0m"; break;
+      // Bold on
+      case 2: *stream_ << "\e[1m"; break;
+      default: std::runtime_error("unkown attr");
+    }
+  }
+ private:
+  int prev_ = -1;
+  std::ostream* stream_;
+};
+
+
+Ascii::Ascii() :p_(new aa_renderparams({47, 33, 2.0, AA_FLOYD_S, 1, 0})) {
+  aa_recommendhidisplay("stdout");
   aa_hardwareparams hp = aa_defparams;
   hp.supported &= ~AA_DIM_MASK;
   hp.supported |= AA_BOLD_MASK;
+//  hp.supported &= ~AA_BOLD_MASK;
   hp.supported &= ~AA_BOLDFONT_MASK;
   hp.supported &= ~AA_EXTENDED;
   hp.supported &= ~AA_EIGHT;
   ctx_ = aa_autoinit(&hp);
   if(!ctx_) throw std::runtime_error("could not initialize aalib");
+  ctx_->driver = &mem_d;
+  ctx_->params.supported |= AA_BOLD_MASK;
 }
 Ascii::~Ascii() {
   aa_close(ctx_);
@@ -44,5 +68,18 @@ void Ascii::displayImage(const cv::Mat* im) {
   }
   aa_render(ctx_, p_.get(), 0, 0, aa_scrwidth(ctx_), aa_scrheight(ctx_));
   aa_flush(ctx_);
-}
 
+  int x, y;
+  int idx = 0;
+  StreamAttr attr(&std::cout);
+  for (y = 0; y < aa_scrheight(ctx_); y++) {
+    for (x = 0; x < aa_scrwidth(ctx_); x++) {
+      attr.set(ctx_->attrbuffer[idx]);
+      std::cout << ctx_->textbuffer[idx];
+      idx++;
+    }
+    std::cout << endl;
+  }
+  std::cout << endl;
+  std::cout << endl;
+}
