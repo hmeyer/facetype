e05ac68fb0f6680e0ee301c8cfc60788e9b38ec0
Henning Meyer <tutmann@gmail.com>
2018-01-05 23:00:29 +0100

    Added Capital Letters and chars hidden behin mod.
    Also migrated to pi zero w (with slightly changed pin layout).

--- typewriter.cc
+++ typewriter.cc
@@ -1,24 +1,23 @@
 #include "typewriter.h"
 #include <iostream>
+#include <cctype>
 #include <unordered_map>
 #include <wiringPi.h>
 
 const int kOutPort[] = {
-//  0, 2, 3, 12, 13, 14, 21, 22
-  0, 1, 2, 3, 4, 5, 6, 7};
+  0, 2, 3, 12, 13, 14, 21, 22};
 
 const int kSelectPort[] = {
-//  23, 24, 25};
-  11, 8, 9};
+  24, 23, 25};
 
-// const int kEnablePin = 7;
-const int kEnablePin = 10;
+const int kEnablePin = 7;
 const int kLShiftPin = 29;
-const int kModPin = 28;
-const int kCodePin = 27;
+const int kCodePin = 28;
+const int kModPin = 27;
 
 // This value almost matches the typewriter speed, it's a bit faster actually.
 const int kKeyPressDurationMs = 50;
+const int kMaxLineChars = 70;
 
 const std::unordered_map<char, int> kAscii2Key = {
 {'m', 0},
@@ -41,7 +40,7 @@ const std::unordered_map<char, int> kAscii2Key = {
 {'ü', 25},
 {'ä', 27},
 {'ß', 29},
-{'\'', 30},
+{'´', 30},
 {'j', 32},
 {'k', 33},
 {'f', 34},
@@ -61,6 +60,7 @@ const std::unordered_map<char, int> kAscii2Key = {
 // 48 DELETE
 // 49 BACK
 {'\r', 51},
+{' ', 52},
 {'o', 56},
 {'l', 57},
 {'p', 58},
@@ -69,6 +69,38 @@ const std::unordered_map<char, int> kAscii2Key = {
 {'9', 61},
 {'0', 62},
 {'-', 63}
+};
+
+const std::unordered_map<char, char> kShiftChars = {
+{'!', '1'},
+{'"', '2'},
+{'§', '3'},
+{'$', '4'},
+{'%', '5'},
+{'&', '6'},
+{'/', '7'},
+{'(', '8'},
+{')', '9'},
+{'=', '0'},
+{'?', 'ß'},
+{'`', '´'},
+{'*', '+'},
+{';', ','},
+{':', '.'},
+{'_', '-'}
+};
+
+const std::unordered_map<char, char> kModChars = {
+{'µ', '1'},
+{'²', '2'},
+{'³', '3'},
+{'£', '4'},
+{'°', '5'},
+{'\'', '6'},
+{'#', '7'},
+{'<', '8'},
+{'>', '9'},
+{'^', '´'},
 };
 
 Typewriter::Typewriter() {
@@ -81,8 +113,10 @@ Typewriter::Typewriter() {
     digitalWrite(pin, HIGH);
     pinMode(pin, OUTPUT);
   }
-  digitalWrite(kEnablePin, HIGH);
-  pinMode(kEnablePin, OUTPUT);
+  for (auto pin : {kEnablePin, kLShiftPin, kModPin, kCodePin}) {
+    digitalWrite(pin, HIGH);
+    pinMode(pin, OUTPUT);
+  }
 }
 
 void Typewriter::press_key(int key) {
@@ -102,10 +136,37 @@ void Typewriter::press_key(int key) {
 }
 
 void Typewriter::print_char(char c, Boldness b) {
-  auto it = kAscii2Key.find(c);
-  if (it != kAscii2Key.end()) {
-    press_key(it->second);
+  bool dead_key = (c == '^' || c == '`' || c == '´');
+
+  if (std::isupper(c)) {
+    enable_shift(true);
+    c = std::tolower(c);
   }
+  auto shift_char_it = kShiftChars.find(c);
+  if (shift_char_it != kShiftChars.end()) {
+    enable_shift(true);
+    delay(kKeyPressDurationMs);
+    c = shift_char_it->second;
+  }
+  auto mod_char_it = kModChars.find(c);
+  if (mod_char_it != kModChars.end()) {
+    enable_mod(true);
+    delay(kKeyPressDurationMs);
+    c = mod_char_it->second;
+  }
+  auto ascii_2_key_it = kAscii2Key.find(c);
+  if (ascii_2_key_it != kAscii2Key.end()) {
+    press_key(ascii_2_key_it->second);
+    line_chars_++;
+    if (ascii_2_key_it->second == 51) line_chars_ = 0;
+    if (line_chars_ > kMaxLineChars) {
+      line_chars_ = 0;
+      press_key(51);
+    }
+  }
+  enable_shift(false);
+  enable_mod(false);
+  if (dead_key) { print_char(' '); }
 }
 
 void Typewriter::print_string(const std::string& s, Boldness b) {
@@ -114,3 +175,14 @@ void Typewriter::print_string(const std::string& s, Boldness b) {
   }
 }
 
+void Typewriter::enable_shift(bool enable) {
+  digitalWrite(kLShiftPin, !enable);
+}
+
+void Typewriter::enable_mod(bool enable) {
+  digitalWrite(kModPin, !enable);
+}
+
+void Typewriter::enable_code(bool enable) {
+  digitalWrite(kCodePin, !enable);
+}
--- typewriter.h
+++ typewriter.h
@@ -11,5 +11,9 @@ class Typewriter {
   void print_char(char c, Boldness b=kNormal); 
   void print_string(const std::string& s, Boldness b=kNormal); 
   void press_key(int key);
+  void enable_shift(bool enable);
+  void enable_mod(bool enable);
+  void enable_code(bool enable);
 private:
+  size_t line_chars_ = 0;
 };
